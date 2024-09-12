8b82be873f93ea348e3d9a81ae9c9458da14f957
Anthony Ho <git@antho.me>
2024-09-11 18:30:22 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'nokogiri'
+gem 'sorbet-runtime'
+
+group :development do
+  gem 'debug'
+  gem 'sorbet'
+  gem 'tapioca', require: false
+end
---
+++ main.rb
@@ -0,0 +1,10 @@
+#! /usr/bin/env ruby
+# typed: true
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'sorbet-runtime'
+extend T::Sig
+
